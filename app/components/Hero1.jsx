cab63fa8503d73641697ddd0cc7b84d4e2bd2981
U220053 <knitu7392@gmail.com>
2024-03-22 13:13:03 +0530

    hero1

--- app/components/Hero1.jsx
+++ app/components/Hero1.jsx
@@ -26,17 +26,17 @@ const Hero = () => {
   };
 
   return (
-    <div className="w-full max-w-screen-xl mx-auto px-6 lg:px-0 bg-bluelight overflow-hidden">
-      <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 lg:gap-16 items-center">
-        <div className="md:mx-auto lg:ml-10">
+    <div className="w-full max-w-screen-xl mx-auto px-6 lg:px-0 rounded-3xl bg-bluelight overflow-hidden">
+      <div className="grid grid-cols-1 lg:grid-cols-2   items-center">
+        <div className="md:mx-auto lg:ml-10 ">
           <div className="mb-8">
-            <p className="text-xl md:text-3xl lg:text-4xl font-poppins leading-normal font-black">
+            <p className="text-xl md:text-4xl lg:text-5xl font-poppins leading-normal font-black mb-2">
               Dive into the World of
             </p>
-            <div className="typewriter-container text-xl md:text-3xl lg:text-4xl">
+            <div className="typewriter-container text-xl md:text-4xl lg:text-5xl mb-2">
               {typewriterLoaded && <TypewriterText />}
             </div>
-            <p className="text-xl md:text-3xl lg:text-4xl font-poppins leading-normal font-black">
+            <p className="text-xl md:text-4xl lg:text-5xl font-poppins leading-normal font-black mb-2">
               Real Estate Investment
             </p>
           </div>
@@ -49,15 +49,25 @@ const Hero = () => {
           <div>
             <button
               onClick={handleOpenModal}
-              className="w-full md:w-[16.25rem] h-[3.8rem] rounded-md bg-black text-white font-bold"
+              className="w-[10rem] h-[2.8rem] md:w-[16.25rem] md:h-[3.8rem] rounded-md bg-black text-white font-bold"
             >
               Join Waitlist
             </button>
           </div>
         </div>
-        <div className="relative h-96 lg:h-auto w-full">
-          <Image src={house} alt="home" />
+        {/* lg:h-[520px] lg:w-[680px] */}
+        <div className="relative h-auto  w-full">
+          <Image
+            src={house}
+            alt="home"
+            layout="responsive"
+            width={550}
+            height={450}
+          />
         </div>
+        {/* <div className="relative h-96 lg:h-auto w-full">
+          <Image src={house} alt="home" />
+        </div> */}
       </div>
       {/* Render the modal only if isModalOpen is true */}
       <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
@@ -95,7 +105,7 @@ const TypewriterText = () => {
 
   const [text, setText] = useState("");
   return (
-    <p className="text-xl md:text-3xl lg:text-4xl font-poppins leading-normal font-black text-darkblue relative top-0 left-0">
+    <p className="text-xl md:text-4xl lg:text-5xl font-poppins leading-normal font-black text-darkblue relative top-0 left-0">
       {text}
     </p>
   );
