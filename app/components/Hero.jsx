63f62ffdbae51e54617de6eb2981ce679da117be
U220053 <knitu7392@gmail.com>
2024-03-26 16:33:57 +0530

    hero.js

--- app/components/Hero.jsx
+++ app/components/Hero.jsx
@@ -1,31 +1,94 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useState } from "react";
 import Image from "next/image";
 import house from ".././assets/house.png";
+import Modal from "./Modal";
+import ModalBody from "./ModalBody";
+import { ReactTyped } from "react-typed";
 
 const Hero = () => {
+  const [typewriterLoaded, setTypewriterLoaded] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  useEffect(() => {
+    import("react-typewriter-effect").then(() => {
+      setTypewriterLoaded(true);
+    });
+  }, []);
+
+  // Function to handle opening the modal
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // Function to handle closing the modal
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
   return (
-    <div className="w-79 h-45 rounded-2xl flex justify-between m-20 bg-bluelight">
-      <div className="w-29 h-24 gap-1 grid grid-cols-1">
-        <div className="w-29 h-12 m-10">
-          <p className="text-5xl font-poppins leading-normal font-black ">
-            Dive into the World of Frictionless Real Estate Investment
-          </p>
+    <div className="w-full max-w-screen-xl mx-auto px-6 lg:px-0 rounded-3xl bg-bluelight overflow-hidden">
+      {/* <div className="grid grid-cols-1 lg:grid-cols-2   items-center"> */}
+      <div className="flex flex-col lg:flex-row items-center justify-between">
+        <div className="md:mx-auto lg:ml-10  lg:max-w-md md:mt-16 max-sm:mt-10">
+          <div className="mb-8 ">
+            <p className="text-3xl md:text-4xl lg:text-42 font-poppins font-black mb-2 lg:leading-normal">
+              Dive into the World of {""}
+              <span className="typewriter-container inline-block  text-3xl md:text-4xl lg:text-42 font-poppins lg:leading-normal font-black text-darkblue relative top-0 left-0">
+                <ReactTyped
+                  strings={[
+                    "Frictionless",
+                    "Simplified",
+                    "Fractionalised",
+                    "Tokenised",
+                  ]}
+                  typeSpeed={150}
+                  backSpeed={100}
+                  loop
+                />
+              </span>
+              {""}
+            </p>
+
+            <p className="text-3xl md:text-4xl lg:text-42 font-poppins lg:leading-normal font-black mb-2">
+              Real Estate Investment
+            </p>
+          </div>
+
+          <div className="mb-8">
+            <p className="text-sm font-poppins leading-normal">
+              Start earning rental yield and capital growth with our
+              property-backed tokenised investments with benefits of DeFi
+            </p>
+          </div>
+          <div>
+            <button
+              onClick={handleOpenModal}
+              className="w-[10rem] h-[2.8rem] md:w-[16.25rem] md:h-[3.8rem] rounded-md bg-black text-white font-bold"
+            >
+              Join Waitlist
+            </button>
+          </div>
         </div>
-        <div className="w-29 h-8 mt-[150px] ml-10">
-          <p className="text-sm font-poppins leading-normal  ">
-            Start earning rental yeild and capital growth with our
-            property-backed tokenised investments with benifits of DeFi
-          </p>
-        </div>
-        <div className="w-15 h-3 mt-20 ml-10">
-          <button className="w-[242px] h-[62px] rounded-md bg-black text-white font-bold ">
-            Invest Now
-          </button>
+
+        <div className="relative h-auto  w-full ">
+          <Image
+            src={house}
+            alt="home"
+            layout="responsive"
+            width={550}
+            height={450}
+          />
         </div>
       </div>
-      <div className="w-50 h-35 ml-25">
-        <Image src={house} alt="home" width={1500} />
-      </div>
+
+      {/* Render the modal only if isModalOpen is true */}
+      <Modal isOpen={isModalOpen} onClose={handleCloseModal}>
+        {/* Add modal content here */}
+
+        <ModalBody onCloseModal={handleCloseModal} />
+      </Modal>
     </div>
   );
 };
